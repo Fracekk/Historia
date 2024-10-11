e2c59b0bc58edf28d2be17f681b24efb344128b0
Fracekk <volionpl@gmail.com>
2024-10-11 20:43:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 $(function() {
     var inputs = $('.input');
     var paras = $('.description-flex-container').find('p');
+    resetState();
+
+    function resetState() {
+        inputs.removeClass('active');
+        paras.removeClass('active');
+        inputs.first().addClass('active'); 
+        paras.first().addClass('active');
+    }
+
+    // Obsługuje kliknięcia na inputy
     inputs.click(function() {
         var t = $(this),
             ind = t.index(),
             matchedPara = paras.eq(ind);
-        
         t.add(matchedPara).addClass('active');
-        inputs.not(t).add(paras.not(matchedPara)).removeClass('active');
+        inputs.not(t).removeClass('active');
+        paras.not(matchedPara).removeClass('active');
     });
 });
